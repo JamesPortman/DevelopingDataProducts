aa6a068a8f5b3ec62f6a54c86568d8c8e7d129de
James Portman <james@portman.ca>
2016-05-15 11:02:53 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,16 +1,30 @@
+# ui.R
+# Date: May 14, 2016
+# Author: James Portman
+
 library(shiny)
 
 shinyUI(
-  navbarPage("Developing Data Products",
+  navbarPage("Motor Trend Analysis",
+            
+             # Panel 1 Provide background documentation.
+             tabPanel("Documentation",
+                      h4("Background"),
+                      "The data is from the 1974 Motor Trend magazine article that lists 10 car specifications for 32 different types of cars. The manual cars in the set of data tend to have better MPG ratings but this can be explained by other specifications such as horsepower and weight.",
+                      hr(),
+                      h4("Original Project"),
+                      helpText(  a("View PDF on GitHub", href="https://github.com/JamesPortman/DevelopingDataProducts/blob/master/DoesTransmissionTypeAffectMPG.pdf"))
+             ),
              
+             # Panel 2 Show a basic histogram of the data with a normal curve superimposed
              tabPanel("Histogram",
                       
                       sidebarPanel(
                         sliderInput("bins",
                                     "Number of bins:",
-                                    min = 1,
-                                    max = 10,
-                                    value = 6)
+                                    min = 5,
+                                    max = 15,
+                                    value = 10)
                       ),
                       
                       mainPanel(
@@ -19,9 +33,10 @@ shinyUI(
                       
              ),
              
+             # Panel 3 Show box plots and fitted regression models.
              tabPanel("Analysis",
                       fluidPage(
-                        titlePanel("Variables affecting miles per gallon (mpg)"),
+                        titlePanel("Variables affecting miles per gallon"),
                         sidebarLayout(
                           sidebarPanel(
                             selectInput("variable", "Variable:",
@@ -45,16 +60,7 @@ shinyUI(
                           )
                         )
                       )
-             ),
-             
-             
-             tabPanel("Documentation",
-                      
-                       h4("Background"),
-                      "The data is from the 1974 Motor Trend magazine article that lists 10 car specifications for 32 different types of cars. The manual cars in the set of data tend to have better MPG ratings but this can be explained by other specifications such as horsepower and weight.",
-                      hr(),
-                      h4("Original Project"),
-                      helpText(  a("Download PDF", href="https://github.com/JamesPortman/DevelopingDataProducts/blob/master/DoesTransmissionTypeAffectMPG.pdf"))
              )
-  )
+    )
 )
+# End ui.R
